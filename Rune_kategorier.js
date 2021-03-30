0957748a00897319d75e47ea14149185aacf0b61
JohanRune <kjgrune@yahoo.se>
2021-03-30 17:44:48 +0200

    Blir 4 i rad

--- Rune_kategorier.js
+++ Rune_kategorier.js
@@ -1,7 +1,22 @@
 // jag kan göra en array med kategorier. En for-loop skapar upp nya event-
 // listeners och lägger 
-document.getElementById("fruit").addEventListener("click", showProducts1);
-//document.getElementById("mejeri").addEventListener("click", showProducts2);
+document.getElementById("fruit").addEventListener("click", showCategory1);
+document.getElementById("mejeri").addEventListener("click", showCategory2);
+/* document.getElementById("").addEventListener("click", showCategory3);
+document.getElementById("").addEventListener("click", showCategory4);
+document.getElementById("").addEventListener("click", showCategory5);
+document.getElementById("").addEventListener("click", showCategory6);
+document.getElementById("").addEventListener("click", showCategory7);
+document.getElementById("").addEventListener("click", showCategory8);
+document.getElementById("").addEventListener("click", showCategory9);
+document.getElementById("").addEventListener("click", showCategory10);
+document.getElementById("").addEventListener("click", showCategory11);
+document.getElementById("").addEventListener("click", showCategory12);
+document.getElementById("").addEventListener("click", showCategory13);
+document.getElementById("").addEventListener("click", showCategory14);
+document.getElementById("").addEventListener("click", showCategory15);
+document.getElementById("").addEventListener("click", showCategory16); */
+
 
 
 
@@ -23,7 +38,74 @@ let data = JSON.parse(localStorage.getItem("products"));  if (!data) {
 // nu måla upp varor med den valda kategorin. 
 //"om produkten har kategori x, måla då upp den"
 
-function showProducts1(){
+function showCategory1(){
+   showAllProductsInCategory("fruit")
+}
+   
+function showCategory2(){
+    showAllProductsInCategory("mejeri")
+}
+
+/* function showCategory3(){
+    showAllProductsInCategory("")
+}
+
+function showCategory4(){
+    showAllProductsInCategory("")
+}
+
+function showCategory5(){
+    showAllProductsInCategory("")
+}
+
+function showCategory6(){
+    showAllProductsInCategory("")
+}
+
+function showCategory7(){
+    showAllProductsInCategory("")
+}
+
+function showCategory8(){
+    showAllProductsInCategory("")
+}
+
+function showCategory9(){
+    showAllProductsInCategory("")
+}
+
+function showCategory10(){
+    showAllProductsInCategory("")
+}
+
+function showCategory11(){
+    showAllProductsInCategory("")
+}
+
+function showCategory12(){
+    showAllProductsInCategory("")
+}
+
+function showCategory13(){
+    showAllProductsInCategory("")
+}
+
+function showCategory14(){
+    showAllProductsInCategory("")
+}
+
+function showCategory15(){
+    showAllProductsInCategory("")
+}
+
+function showCategory16(){
+    showAllProductsInCategory("")
+} */
+
+
+
+function showAllProductsInCategory(category){
+   
     let output = "";
     let counter = 1;
 
@@ -32,54 +114,29 @@ function showProducts1(){
             counter = 1;
             console.log(counter)
         }
-        
-        if (allProductsArray[index].category == `fruit`){
-                
+           
+        if (allProductsArray[index].category == category){
             if (counter==1) {
-                console.log("hallå")
-                output += `<div class="row">`};
+            output += `<div class="row">`};
+        
             output +=
-            `<div class="col-md-4">`
+            `<div class="col-md-3">`
             + "<hr> <h6>" + allProductsArray[index].title + "</h6><br>"
+            //+ "<img src=" + allProductsArray[index].image + " width=\"100\"> <br>"
             + allProductsArray[index].price + " kr <br>"
             + `<button id="product${allProductsArray[index].id}" >Buy</button> <br>`
             + "</div>"; 
-            if (index==4) {
-                console.log("hallå igen");
+            
+            if (counter==4) {
                 output += "</div>"};
+            counter += 1;
 
         }
-        counter += 1;
+       
     }
     console.log(output)
     document.getElementById("products").innerHTML = output;
 }
 
-function showProducts2(){
 
-}
 
-/* 
-<div class="container">
-<div class="row"> 
-    <div id=0 class="col-md-4"><br><button id="button1">Add to basket</button></div>
-    <div id=1 class="col-md-4"><br><button id="button2">Add to basket</button></div>
-    <div id=2 class="col-md-4"><br><button id="button3">Add to basket</button></div>
-</div>
-<div class="row"> 
-    <div id=3 class="col-md-4"><br><button id="button4">Add to basket</button></div>
-    <div id=4 class="col-md-4"><br><button id="button5">Add to basket</button></div>
-    <div id=5 class="col-md-4"><br><button id="button6">Add to basket</button></div>
-</div>
-<div class="row"> 
-    <div id=6 class="col-md-4"><br><button id="button7">Add to basket</button></div>
-    <div id=7 class="col-md-4"><br><button id="button8">Add to basket</button></div>
-    <div id=8 class="col-md-4"><br><button id="button9">Add to basket</button></div>
-</div>
-<div class="row"> 
-    <div id=9 class="col-md-4"><br><button id="button10">Add to basket</button></div>
-    <div id=10 class="col-md-4"><br><button id="button11">Add to basket</button></div>
-    <div id=11 class="col-md-4"><br><button id="button12">Add to basket</button></div>
-</div>
-</div>
- */
