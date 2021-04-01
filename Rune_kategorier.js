8d5402fe1327f4227c6811498f31f557bc59b434
JohanRune <kjgrune@yahoo.se>
2021-04-01 08:21:16 +0200

    Funkar med cards

--- Rune_kategorier.js
+++ Rune_kategorier.js
@@ -18,7 +18,6 @@ document.getElementById("").addEventListener("click", showCategory15);
 document.getElementById("").addEventListener("click", showCategory16); */
 
 
-//localStorage.clear();
 
 let data = JSON.parse(localStorage.getItem("products"));  
 if (!data) {   
@@ -36,9 +35,6 @@ if (!data) {
          console.log(allProductsArray);
 
 
-// leta igenom databasen efter varor med rätt kategori.
-// nu måla upp varor med den valda kategorin. 
-//"om produkten har kategori x, måla då upp den"
 
 function showCategory1(){
    showAllProductsInCategory("fruit")
@@ -106,8 +102,6 @@ function showCategory16(){
 
 
 
-
-
 function showAllProductsInCategory(category){
    
     let output = "";
@@ -122,89 +116,30 @@ function showAllProductsInCategory(category){
 
         if (allProductsArray[index].category == category){
             if (counter==1) {
-            output += `<div class="row">`};
-        
-            output +=
-            `<div class="col-md-3">`
-            + "<hr> <img src=" + allProductsArray[index].image + " width=\"100\"> <br>"
-            + allProductsArray[index].price + " kr <br>"
-            + "<h6>" + allProductsArray[index].title + "</h6><br>"
-            + `<button id="product${allProductsArray[index].id}" >Köp</button> <br>`
-            + "</div>"; 
+                output += `<div class="row">`};
             
-            if (counter==4) {
-                output += "</div>"};
-            counter += 1;
-
-        }
-       
+                output +=
+                `<div class="col-lg-3 col-md-3 mb-3">`
+                    +`<div class="card" style="width:150px">`
+                        + `<div class="card-body text-center">`
+                            + `<img class="card-img-top" src=` + allProductsArray[index].image + `>`
+                            + `<p class="card-text">Pris `+ allProductsArray[index].price +` kr</p>`
+                            + `<h6 class="card-title">` + allProductsArray[index].title + "</h6>"
+                            + `<a class="btn btn-primary" id="product${allProductsArray[index].id}">Köp</a>`
+                        + "</div>"
+                    + "</div>"
+                + "</div>"; 
+                
+                if (counter==4) {
+                    output += "</div>"};
+                counter += 1;
+        } 
     }
     console.log(output)
     document.getElementById("products").innerHTML = output;
 }  
 
 
-// ska den slumpa ut bilder?
-// Visa 7 rader
-// slumpa. random nummer 1-15. om tex nummer 3 dras, visa då produkt med id 3 på plats 1. 
-// en array med slumpade nummer 1-15
-// en annan array med de nummer som redan har dragits. 
-// sedan måla ut en produkt om den inte finns i arrayen med dragna nummer. 
-// särskilda metoder för att söka igenom array.
-// alternativ: slumpa 15 tal och lägg i en array. Ta sedan index 0 till första platsen,
-// och index 1 till andra platsen osv. Detta är bättre. 
-
-
-//localStorage.clear();
-/* 
-showProducts();
-
-function showProducts (){
-   
-    let randomProductsArray = [];
-    randomProductsArray = randomizer();
-
-    let output = "";
-    let counter = 1;
-
-    for (let index = 0; index < 15; index++) {
-        if (counter == 5){
-            counter = 1;
-            console.log(counter)
-        }
-        
-        let index2 = 0;
-        index2 = randomProductsArray[index]
-        console.log(index2);
-
-
-        
- 
-    
-        output +=
-        `<div class="col-lg-3 col-md-3 mb-3">`
-        + `<div class="card h-100"`
-        + `<img class="card-img-top" src=` + allProductsArray[index2].image + " width=\"100\"> "
-        + `<div class="card-body">`
-        + `<h6 class="card-title">` + allProductsArray[index2].title + `</h6>`
-        + `<p class="card-text">` + allProductsArray[index2].price + " kr </p>"
-        + `<button class="card-button" 
-        id="product${allProductsArray[index2].id}" >Köp</button>`
-        + "</div>"
-        + "</div>"
-        + "</div>"; 
-        
-    
-
-    }
-    console.log(output)
-    document.getElementById("products").innerHTML = output;
-}
- */
-
-
-
-//detta visar upp produkterna fungerande men utan cards. Fungerar
 showProducts();
 
 function showProducts(){
@@ -215,13 +150,14 @@ function showProducts(){
     let output = "";
     let counter = 1;
 
+    output += `<div class="container">`
+
     for (let index = 0; index < 15; index++) {
         if (counter == 5){
             counter = 1;
             console.log(counter)
         }
            
-
         let index2 = 0;
         index2 = randomProductsArray[index]
 
@@ -230,70 +166,27 @@ function showProducts(){
             output += `<div class="row">`};
         
             output +=
-            `<div class="col-md-3">`
-            + "<hr><img src=" + allProductsArray[index2].image + " width=\"100\"> <br>"
-            + allProductsArray[index2].price + " kr <br>"
-            + " <h6>" + allProductsArray[index2].title + "</h6><br>"
-            + `<button id="product${allProductsArray[index2].id}" >Köp</button> <br>`
+            `<div class="col-lg-3 col-md-3 mb-3">`
+                +`<div class="card" style="width:150px">`
+                    + `<div class="card-body text-center">`
+                        + `<img class="card-img-top" src=` + allProductsArray[index2].image + `>`
+                        + `<p class="card-text">Pris `+ allProductsArray[index2].price +` kr</p>`
+                        + `<h6 class="card-title">` + allProductsArray[index2].title + "</h6>"
+                        + `<a class="btn btn-primary" id="product${allProductsArray[index2].id}">Köp</a>`
+                    + "</div>"
+                + "</div>"
             + "</div>"; 
             
             if (counter==4) {
                 output += "</div>"};
             counter += 1;
-
-
     }
-    console.log(output)
-    document.getElementById("products").innerHTML = output;
-}  
 
-/* 
-showProducts2();
-
-function showProducts2(){
+    output += "</div>"
    
-    let randomProductsArray = [];
-    randomProductsArray = randomizer();
-
-    let output = "";
-    let counter = 1;
-
-    for (let index = 0; index < 15; index++) {
-        if (counter == 5){
-            counter = 1;
-            console.log(counter)
-        }
-           
-
-        let index2 = 0;
-        index2 = randomProductsArray[index]
-
-
-            if (counter==1) {
-            output += `<div class="row">`};
-        
-            output +=
-            `<div class="col-md-3">`
-            + "<hr><img src=" + allProductsArray[index2].image + " width=\"100\"> <br>"
-            + " <h6>" + allProductsArray[index2].title + "</h6><br>"
-            + allProductsArray[index2].price + " kr <br>"
-            + `<button id="product${allProductsArray[index2].id}" >Köp</button> <br>`
-            + "</div>"; 
-            
-            if (counter==4) {
-                output += "</div>"};
-            counter += 1;
-
-
-    }
     console.log(output)
     document.getElementById("products").innerHTML = output;
 }  
-
- */
-
-
-
 
 
 
