f5950685bbce87f17fc7d7dc79ddd4b8c6090b9a
Axel Jeansson <axel.jeansson@hotmail.se>
2021-04-01 11:31:15 +0200

    Uppdaterat footer och login

--- js/ui/modules/login.js
+++ js/ui/modules/login.js
@@ -4,38 +4,66 @@ export default class Login extends UI {
     constructor(appendTo) {
         super(appendTo);
         this.html = `
-        <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered" role="document">
+        <div
+      class="modal fade"
+      id="loginModal"
+      tabindex="-1"
+      role="dialog"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
-            <div class="modal-header border-bottom-0">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">×</span>
-            </button>
-            </div>
-            <div class="modal-body">
+          <div class="modal-header border-bottom-0">
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
             <!--<div class="modal-body2">-->
             <div class="form-title text-center">
-                <h4>Logga in</h4>
+              <h4>Logga in</h4>
             </div>
             <div class="d-flex flex-column text-center">
-                <form name="myForm" action="" method="post" onsubmit="validateUser()">
+              <form>
                 <div class="form-group">
-                    <input type="email" class="form-control" id="email1" placeholder="Ange email...">
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="email1"
+                    placeholder="Ange email..."
+                  />
                 </div>
                 <div class="form-group">
-                    <br>
-                    <input type="password" class="form-control" id="password1" placeholder="Ange lösenord...">
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="password1"
+                    placeholder="Ange lösenord..."
+                  />
                 </div>
-                <br>
-                <button type="button" id="login-btn" class="btn btn-info btn-block btn-round">Logga in</button>
-                </form>
+              
+                <button
+                  type="button"
+                  onclick="validateUser()"
+                  class="btn btn-info btn-block btn-round"
+                >
+                  Logga in
+                </button>
+              </form>
             </div>
-            </div>
+          </div>
         </div>
+      </div>
+      <div class="modal-footer d-flex justify-content-center">
+        <div class="signup-section">
+          Not a member yet? <a href="#a" class="text-info"> Sign Up</a>.
         </div>
-        </div>
-        `;
+      </div>
+    </div>`
         super.container.innerHTML = this.html;
     }
 }
