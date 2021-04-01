30cb26c5cd04931a5adfd338b749f0ddb1f1fb2e
Max J <14187946+m4xiJo@users.noreply.github.com>
2021-04-01 09:59:51 +0200

    submit missing stuff (Thanx GitHub)

---
+++ js/index.js
@@ -0,0 +1,19 @@
+/**
+ * Fetch the data from the JSON file and pushes it to Local Storage
+ * 
+ */
+let data = JSON.parse(localStorage.getItem("products"));
+
+if (!data) {
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", "produkter.JSON");
+  xhr.send();
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      let data = JSON.parse(xhr.responseText);
+      localStorage.setItem("products", JSON.stringify(data));
+      console.log(data);
+    }
+  };
+}
+
