1f20179a590932cf0fa53ef3732ba2de74f20654
peteralmgren <72129435+peteralmgren@users.noreply.github.com>
2021-04-06 14:38:14 +0200

    fix with localstorage customer

--- js/addProduct.js
+++ js/addProduct.js
@@ -14,8 +14,6 @@ $(document).ready(function() {
             newProductArray= [];
             
           }
-        /* console.log(formData);
-        console.log(jsonData); */
      
      $.each(formData, function() {
           if (jsonData[this.name]) {
@@ -26,12 +24,7 @@ $(document).ready(function() {
          } else {
              jsonData[this.name] = this.value || '';
          }
-         console.log(formData[0].value);
-
          
-
-         /* newProductArray= JSON.parse(localStorage.getItem("newProducts")) */
-         console.log(newProductArray);
 
             newProduct = {
             "id": formData[0].value,
@@ -41,44 +34,13 @@ $(document).ready(function() {
             "price": formData[4].value,
             "category": formData[1].value 
             }
-        console.log(newProduct);
         
         
         
-        
-        
-        /* newProduct.array.forEach(element => {
-            console.log("ny produkt: " + element);
-        }); */
-             
-         
-         
-
-         /* const fs = require("fs");
-         let usersjson = fs.readFileSync("data/produkter.json","utf-8");
-
-         let products = JSON.parse(usersjson);
-
-         product.push(newProduct);
-
-         usersjson = JSON.stringify(products);
-
-         fs.writeFileSync("produkter.json",usersjson,"utf-8"); */
-
-
      });
      newProductArray.push(newProduct);
      localStorage.setItem("newproduct", JSON.stringify(newProductArray));
-     /* jsonData.forEach(element => {
-         console.log("Element: " + element);
-     }) */
      
-
-     /* for (let i = 0; i < jsonData.length; i++) {
-        let element = jsonData[i];
-        console.log("element i jsonarray: "+element);
-        
-    } */
 
      
       $.ajax(
@@ -94,34 +56,4 @@ $(document).ready(function() {
   
 
 
-  function export2txt() {
-      console.log("i export 2 file");
-    const originalData = {
-      members: [{
-          name: "cliff",
-          age: "34"
-        },
-        {
-          name: "ted",
-          age: "42"
-        },
-        {
-          name: "bob",
-          age: "12"
-        }
-      ]
-    };
-    
-    let oldProdukter = JSON.parse(localStorage.getItem("products"));
-    const a = document.createElement("a");
-    a.href = URL.createObjectURL(new Blob([JSON.stringify(originalData, null, 2)], {
-      type: "text/plain"
-    }));
-    
-    a.setAttribute("nyfil", "data");
-    document.body.appendChild(a);
-
-    /* a.save("./data"); */
-    a.click();
-    document.body.removeChild(a);
-  }
+  
