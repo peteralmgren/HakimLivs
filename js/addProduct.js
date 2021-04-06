0f782bd776088556fa283bfd94958ffffe8709a4
Axel Jeansson <axel.jeansson@hotmail.se>
2021-04-06 15:52:10 +0200

    pulled from newDevelop

--- js/addProduct.js
+++ js/addProduct.js
@@ -1,9 +1,12 @@
-
 $(document).ready(function() {
-  $("#add-btn").click(function(e){
-     var jsonData = {};
+    $("#add-btn").click(function(e){
+       var jsonData = {};
+       
+       
+     var formData = $("#myform").serializeArray();
+     let newProduct = "";
+     let newProductArray = [];
      
-<<<<<<< HEAD
          if(JSON.parse(localStorage.getItem("newproduct"))){
             newProductArray = JSON.parse(localStorage.getItem("newproduct"));
           }else{
@@ -36,32 +39,8 @@ $(document).ready(function() {
      });
      newProductArray.push(newProduct);
      localStorage.setItem("newproduct", JSON.stringify(newProductArray));
-=======
->>>>>>> bb22387d20cecb8406def6060efb917126261ec3
      
-   var formData = $("#myform").serializeArray();
-   let newProduct = "";
-   let newProductArray = [];
-   
-       if(JSON.parse(localStorage.getItem("newproduct"))){
-          newProductArray = JSON.parse(localStorage.getItem("newproduct"));
-        }else{
-          newProductArray= [];
-          
-        }
-   
-   $.each(formData, function() {
-        if (jsonData[this.name]) {
-           if (!jsonData[this.name].push) {
-               jsonData[this.name] = [jsonData[this.name]];
-           }
-           jsonData[this.name].push(this.value || '');
-       } else {
-           jsonData[this.name] = this.value || '';
-       }
-       
 
-<<<<<<< HEAD
      
       $.ajax(
       {
@@ -70,42 +49,6 @@ $(document).ready(function() {
           data : jsonData,
           
       });
-      e.preventDefault();	
+      e.preventDefault();    
   }); 
   });
-  
-
-
-  
-=======
-          newProduct = {
-          "id": formData[0].value,
-          "title": formData[2].value,
-          "description": formData[3].value,
-          "image": formData[5].value,
-          "price": formData[4].value,
-          "category": formData[1].value 
-          }
-      
-      
-      
-   });
-   newProductArray.push(newProduct);
-   localStorage.setItem("newproduct", JSON.stringify(newProductArray));
-   
-
-   
-    $.ajax(
-    {
-        url : "action.php",
-        type: "POST",
-        data : jsonData,
-        
-    });
-    e.preventDefault();	
-}); 
-});
-
-
-
->>>>>>> bb22387d20cecb8406def6060efb917126261ec3
