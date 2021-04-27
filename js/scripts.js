05643d63d14fafba6cc270eb679fe67da6037606
peteralmgren <72129435+peteralmgren@users.noreply.github.com>
2021-04-27 08:24:23 +0200

    login fix

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,29 @@
         $('[data-toggle="tooltip"]').tooltip()
       })
     }); */
-    customerLoggingIn = []
+    
 
 function validateUser(){
   var correctEmail = "hakim";
   var correctPassword = "hakim";
   var email = document.getElementById("email1").value;
   var password = document.getElementById("password1").value;
-  customerLoggingIn.push(email)
-  customerLoggingIn.push(password)
-  customerLoggingIn = JSON.stringify
-  console.log(customerLoggingIn);
+  
+  const xhr = new XMLHttpRequest();
+    xhr.open("GET", `https://grupp5hakimlivs.herokuapp.com/login?email=${email}&password=${password}`);
+    xhr.send();
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        let data = JSON.parse(xhr.responseText);
+        sessionStorage.setItem("loggedinCustomer", JSON.stringify(data));
+        console.log(data);
+      }
+    };
+
+    let loggedinCustomer = JSON.parse(sessionStorage.getItem("loggedinCustomer"));
+    console.log(loggedinCustomer);
+
+
   /* console.log("input email: " + email);
   console.log("input password: " + password); */
 
