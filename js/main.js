00fab40c9378e548138e5ca63d03255c3e18097f
aJeann <axel.jeansson@hotmail.se>
2021-09-03 14:15:20 +0200

    removed fake login

--- js/main.js
+++ js/main.js
@@ -18,21 +18,3 @@ window.addEventListener("click", async (e) => {
     if(e.target.id == "login-btn") validateUser();
 });
 
-function loginAction() {
-    window.location()
-}
-
-function validateUser() {
-    var correctEmail = "hakim";
-    var correctPassword = "hakim";
-    var email = document.getElementById("email1").value;
-    var password = document.getElementById("password1").value;
-    console.log("input email: " + email);
-    console.log("input password: " + password);
-  
-    if(email == correctEmail && password == correctPassword){
-        console.log("Användarnamn och lösenord stämde!");
-        window.location.replace("admin.html");
-    }
-    else console.log("Felaktig adress. Användaren finns ej!");
-}
