d9630f42e83bb49d98276fc5ad5beafed611aef0
Max J <14187946+m4xiJo@users.noreply.github.com>
2021-09-10 21:48:35 +0200

    added paypal payment button

--- cartjs/ui/modules/checkout.js
+++ cartjs/ui/modules/checkout.js
@@ -56,12 +56,13 @@ export default class Checkout extends UI {
               <hr />
               <div class="container-fluid"></div>
               <hr />
+              <div id="paypal-button-container"></div>
+
               <button
-                type="button"
-                id="Purchase"
-                class="purchase-button btn btn-primary btn-lg t-1 hover-shadow"
-              >
-                Bekräfta beställning
+              type="button"
+              id="Purchase"
+              class="purchase-button btn btn-primary btn-lg t-1 hover-shadow"
+              >Bekräfta beställning
               </button>
               <button class="btn btn-danger" id="clearCart">Rensa varukorg</button>
               <button
@@ -107,9 +108,40 @@ export default class Checkout extends UI {
     
     this.injectRowItemsInCart();
     this.printUser();
-    this.sum = 0;         
-    
+    this.sum = 0;
+    this.insertPayPalWidget();
   }
+
+insertPayPalWidget() {
+  paypal.Buttons({   
+    // Sets up the transaction when a payment button is clicked
+    createOrder: function(data, actions) {
+      return actions.order.create({
+        purchase_units: [{
+          amount: {
+            value: '77.44' // Can reference variables or functions. Example: \`value: document.getElementById('...').value\`
+          }
+        }]
+      });
+    },
+
+    // Finalize the transaction after payer approval
+    onApprove: function(data, actions) {
+      return actions.order.capture().then(function(orderData) {
+        // Successful capture! For dev/demo purposes:
+            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
+            var transaction = orderData.purchase_units[0].payments.captures[0];
+            alert('Transaction '+ transaction.status + ': ' + transaction.id + '\n\nSee console for all available details');
+
+        // When ready to go live, remove the alert and show a success message within this page. For example:
+        // var element = document.getElementById('paypal-button-container');
+        // element.innerHTML = '';
+        // element.innerHTML = '<h3>Thank you for your payment!</h3>';
+        // Or go to another URL:  actions.redirect('thank_you.html');
+      });
+    }
+  }).render('#paypal-button-container');
+}
 
 async injectRowItemsInCart() {
     let allProducts = await super.loadData("GET", "https://hakimlivsgroup5.herokuapp.com/all");
