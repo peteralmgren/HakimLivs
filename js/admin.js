037706b220ad45a9f5d03075d1d39ccd73d2c2cb
aJeann <axel.jeansson@hotmail.se>
2021-09-13 21:27:33 +0200

    p

--- js/admin.js
+++ js/admin.js
@@ -299,7 +299,7 @@ $('#show-orders').click(function (e) {
                 },
                  type: "POST",
                  crossDomain: true,
-                 dataType: 'jsonp',
+                 dataType: 'json',
                  data : jsonData,
                  async: true,
                   success : function(response) {
