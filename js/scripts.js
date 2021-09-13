073c2bdacbaf590e1e2b5411b6de1890619db100
aJeann <axel.jeansson@hotmail.se>
2021-09-13 21:48:51 +0200

    decode

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+import jwt_decode from 'jwt-decode';
+
 function validateUser() {
   
 
@@ -23,8 +25,14 @@ function validateUser() {
         alert("Kontot hittades inte!")        
       }
       if(xhr.responseText){
-        let data = JSON.parse(xhr.responseText);
+      
+      let data = JSON.parse(xhr.responseText);
       sessionStorage.setItem("loggedinCustomer", JSON.stringify(data));
+
+      let token = JSON.stringify(data);
+
+      let decoded = jwt_decode(token);
+      console.log(decoded);
      
       if (data.email === "hakim@hakim") {
         window.location.replace("admin.html");
@@ -41,6 +49,16 @@ function validateUser() {
     let btn = document.getElementById("Log-in-button");
     btn.hidden ^= true;
   }
+
+  function parseJwt (token) {
+    var base64Url = token.split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(jsonPayload);
+};
 
 }
 
