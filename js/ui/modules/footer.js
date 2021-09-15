61a0345b3083a0827ef09c971a4859338001d0dd
aJeann <axel.jeansson@hotmail.se>
2021-09-15 11:46:03 +0200

    ÖPPTE

--- js/ui/modules/footer.js
+++ js/ui/modules/footer.js
@@ -41,7 +41,7 @@ export default class Footer extends UI {
               
                         <!--Grid column-->
                         <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-                          <h6 class="text">Öppettider</h6>
+                        <h5 class="text-uppercase">Öppettider</h5>
                           <h6 class="text">Vardagar 10-19</h6>
                           <h6 class="text">Helger 10-18</h6>
         
